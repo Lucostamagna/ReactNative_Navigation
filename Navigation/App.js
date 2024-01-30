760bf32161853833fcb4ca79809cab7fbd1f7868
Lucía Costamagna <luci3220@hotmail.com>
2024-01-29 22:43:14 -0300

    create store and provider

--- Navigation/App.js
+++ Navigation/App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import RootNavigator from './src/Navigation/RootNavigator';
+import { Provider } from 'react-redux';
+import { store } from './src/app/store';
 
 export default function App() {
-  return <RootNavigator />;
-    
-  
+  return (
+    <Provider store={store}>
+      <RootNavigator/>
+    </Provider>
+  );
 }
-
-
--- Navigation/src/Navigation/RootNavigator.js
+++ Navigation/src/Navigation/RootNavigator.js
@@ -1,11 +1,11 @@
 import { NavigationContainer } from '@react-navigation/native';
 import MyStack from './MyStack';
-import MyBottomTab from './MyBottomTab';
-import SingUp from '../Screens/AuthScreens/SingUp';
+
 import AuthStack from './AuthStack';
+import { useSelector } from 'react-redux'
 
 export default function RootNavigator() {
-  const user = false;
+  const user = useSelector(state => state.auth); //el nombre de mi reducer es auth 
   return (
     <NavigationContainer>
       {user ? <MyStack /> : <AuthStack />}
