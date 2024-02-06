085af3fbb7c6889e8005620e9469a41b3071b3d5
Lucía Costamagna <luci3220@hotmail.com>
2024-02-06 00:51:16 -0300

    create onboardingScreen

--- Navigation/src/Screens/HomeScreen.js
+++ Navigation/src/Screens/HomeScreen.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import * as React from 'react';
 import { globalStyles } from '../Styles/Global';
 import { Text, View, Button, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+//chequeo si es la primera vez que el user ingresa
+ React.useEffect(()=>{
+  checkFirstLaunch();
+ },[]);
+ async function checkFirstLaunch(){
+  const firstLaunch= await AsyncStorage.getItem('@firstLaunch');
+  if(firstLaunch){
+    return;
+  }else{
+    await AsyncStorage.setItem('@firstLaunch', 'true');
+    navigation.navigate('OnBoarding');
+  }
+ }
+
+
+
+  
   return (
     <View style={globalStyles.screenContainer}>
       <Text style={globalStyles.title}>HOME</Text>
-      <TouchableOpacity
+      {/* <TouchableOpacity
         style={globalStyles.botton}
         onPress={() => navigation.navigate('Setting')}
       >
@@ -20,7 +38,7 @@ const HomeScreen = () => {
         onPress={() => navigation.navigate('Profile')}
       >
         <Text> Go to Profile</Text>
-      </TouchableOpacity>
+      </TouchableOpacity> */}
     </View>
   );
 };
